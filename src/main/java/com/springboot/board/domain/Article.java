3bd8419dbcefaf0ca474e411dbbcc11087bbe1a0
이소영 <th42500@naver.com>
2022-11-10 12:54:45 +0900

    [Feat] Article Entity 클래스 생성

---
+++ src/main/java/com/springboot/board/domain/Article.java
@@ -0,0 +1,27 @@
+package com.springboot.board.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "article")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
