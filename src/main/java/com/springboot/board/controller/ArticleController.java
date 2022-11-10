c929d825c9635564b95805865bb5a5b7b2f7e91b
이소영 <th42500@naver.com>
2022-11-10 13:06:03 +0900

    [Feat] ArticleController - 게시글 상세정보 조회 기능 추가
    1. Optional을 이용한 null 체크 줄이기
    2. 원하는 값이 없을 경우 error 페이지로 전환

--- src/main/java/com/springboot/board/controller/ArticleController.java
+++ src/main/java/com/springboot/board/controller/ArticleController.java
@@ -5,9 +5,13 @@ import com.springboot.board.domain.dto.ArticleDto;
 import com.springboot.board.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -31,5 +35,18 @@ public class ArticleController {
         Article savedArticle = articleRepository.save(articleDto.toEntity());
         log.info("generatedId: {}", savedArticle.getId());
         return "";
+    }
+
+    @GetMapping("/{id}")
+    public String selectArticle(@PathVariable Long id, Model model) {
+        Optional<Article> optArticle = articleRepository.findById(id);
+
+        if(!optArticle.isEmpty()) {
+            model.addAttribute("article", optArticle.get());
+            return "articles/show";
+        } else {
+            model.addAttribute("message", String.format("%d번 게시글이 존재하지 않습니다.", id));
+            return "error";
+        }
     }
 }
