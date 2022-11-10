80f0133bebb805afbadff9935a088a7e2706076f
이소영 <th42500@naver.com>
2022-11-10 13:02:13 +0900

    [Feat] ArticleController - 게시글 등록 기능 추가
    1. logger를 이용한 ArticleDto 확인
    2. ArticleRepository 의존성 주입
    3. logger를 이용한 (ArticleRepository를 통해) 저장된 Entity 객체 id 확인

--- src/main/java/com/springboot/board/controller/ArticleController.java
+++ src/main/java/com/springboot/board/controller/ArticleController.java
@@ -1,8 +1,12 @@
 package com.springboot.board.controller;
 
+import com.springboot.board.domain.Article;
+import com.springboot.board.domain.dto.ArticleDto;
+import com.springboot.board.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 public class ArticleController {
 
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping("/register")
     public String registerPage() {
         return "article/register";
+    }
+
+    @PostMapping("")
+    public String registArticles(ArticleDto articleDto) {
+        log.info(articleDto.toString());
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        log.info("generatedId: {}", savedArticle.getId());
+        return "";
     }
 }
