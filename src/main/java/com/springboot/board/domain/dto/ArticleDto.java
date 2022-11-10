c4b79d650499afc56cee5584c08dde2e031c2608
이소영 <th42500@naver.com>
2022-11-10 12:55:25 +0900

    [Feat] ArticleDto 클래스 생성

---
+++ src/main/java/com/springboot/board/domain/dto/ArticleDto.java
@@ -0,0 +1,20 @@
+package com.springboot.board.domain.dto;
+
+import com.springboot.board.domain.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class ArticleDto {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public Article toEntity() {
+        return new Article(this.id, this.title, this.content);
+    }
+}
