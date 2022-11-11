408a3f3ee00e4b27b4df55dd23efdb4c9c314bc7
이소영 <th42500@naver.com>
2022-11-11 13:40:46 +0900

    [Refactor] 사용하지 않는 변수 제거

--- src/main/java/com/springboot/board/domain/Hospital.java
+++ src/main/java/com/springboot/board/domain/Hospital.java
@@ -3,6 +3,7 @@ package com.springboot.board.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -15,38 +16,15 @@ import java.time.LocalDateTime;
 public class Hospital {
     @Id
     private Integer id;  // Long --> BigInt
-    private String openServiceName;
-    private int openLocalGovernmentCode;
-    private String managementNumber;
-    private LocalDateTime licenseDate;
-    private int businessStatus;
-    private int businessStatusCode;
-    private String phone;
-    private String fullAddress;
+    @Column(name = "road_name_address")
     private String roadNameAddress;
+    @Column(name = "hospital_name")
     private String hospitalName;
-    private String businessTypeName;
-    private int healthcareProviderCnt;
-    private int patientRoomCnt;
-    private int totalNumberOfBeds;
-    private float totalAreaSize;
 
-    public Hospital(Integer id, String openServiceName, int openLocalGovernmentCode, String managementNumber, LocalDateTime licenseDate, int businessStatus, int businessStatusCode, String phone, String fullAddress, String roadNameAddress, String hospitalName, String businessTypeName, int healthcareProviderCnt, int patientRoomCnt, int totalNumberOfBeds, float totalAreaSize) {
+
+    public Hospital(Integer id, String roadNameAddress, String hospitalName) {
         this.id = id;
-        this.openServiceName = openServiceName;
-        this.openLocalGovernmentCode = openLocalGovernmentCode;
-        this.managementNumber = managementNumber;
-        this.licenseDate = licenseDate;
-        this.businessStatus = businessStatus;
-        this.businessStatusCode = businessStatusCode;
-        this.phone = phone;
-        this.fullAddress = fullAddress;
         this.roadNameAddress = roadNameAddress;
         this.hospitalName = hospitalName;
-        this.businessTypeName = businessTypeName;
-        this.healthcareProviderCnt = healthcareProviderCnt;
-        this.patientRoomCnt = patientRoomCnt;
-        this.totalNumberOfBeds = totalNumberOfBeds;
-        this.totalAreaSize = totalAreaSize;
     }
 }
--- src/main/java/com/springboot/board/domain/dto/HospitalDto.java
+++ src/main/java/com/springboot/board/domain/dto/HospitalDto.java
@@ -12,23 +12,10 @@ import java.time.LocalDateTime;
 @ToString
 public class HospitalDto {
     private int id;
-    private String openServiceName;
-    private int openLocalGovernmentCode;
-    private String managementNumber;
-    private LocalDateTime licenseDate;
-    private int businessStatus;
-    private int businessStatusCode;
-    private String phone;
-    private String fullAddress;
     private String roadNameAddress;
     private String hospitalName;
-    private String businessTypeName;
-    private int healthcareProviderCnt;
-    private int patientRoomCnt;
-    private int totalNumberOfBeds;
-    private float totalAreaSize;
 
     public Hospital toEntity() {
-        return new Hospital(this.id, this.openServiceName, this.openLocalGovernmentCode, this.managementNumber, this.licenseDate, this.businessStatus, this.businessStatusCode, this.phone, this.fullAddress, this.roadNameAddress, this.hospitalName, this.businessTypeName, this.healthcareProviderCnt, this.patientRoomCnt, this.totalNumberOfBeds, this.totalAreaSize);
+        return new Hospital(this.id, this.roadNameAddress, this.hospitalName);
     }
 }
