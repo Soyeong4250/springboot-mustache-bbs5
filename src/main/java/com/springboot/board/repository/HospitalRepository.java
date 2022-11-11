8363bd41d7428eb8a6b35e24c7d74bd269de2627
이소영 <th42500@naver.com>
2022-11-11 14:55:43 +0900

    [Refactor] 페이지 기능 구현을 위한 HospitalRepository 수정

--- src/main/java/com/springboot/board/repository/HospitalRepository.java
+++ src/main/java/com/springboot/board/repository/HospitalRepository.java
@@ -1,7 +1,12 @@
 package com.springboot.board.repository;
 
 import com.springboot.board.domain.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    Page<Hospital> findAll(Pageable pageable);
 }
