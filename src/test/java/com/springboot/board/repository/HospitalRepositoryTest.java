44e88a493ec10ddc48892fb76a6a9f6122108e05
이소영 <th42500@naver.com>
2022-11-11 13:07:31 +0900

    [Test] Hospital 정보를 잘받아오는지 테스트하는 getHospital() 작성
    테스트 결과 성공

---
+++ src/test/java/com/springboot/board/repository/HospitalRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.springboot.board.repository;
+
+import com.springboot.board.domain.Hospital;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HospitalRepositoryTest {
+
+    @Autowired
+    HospitalRepository hospitalRepository;
+
+    @Test
+    @DisplayName("Hospital 정보를 잘 가져오는지 테스트")
+    void getHospital() {
+        hospitalRepository.findById(1);
+        Optional<Hospital> hospital = hospitalRepository.findById(1);
+        Hospital hp = hospital.get();
+        System.out.println(hp.getId());
+        assertEquals(1, hp.getId());
+    }
+
+}
