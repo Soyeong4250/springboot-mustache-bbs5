39b1f89da4c4894f32a5d55cffce11da162204a0
이소영 <th42500@naver.com>
2022-11-14 14:45:03 +0900

    [Add] HospitalRepository - JPQL을 이용한 Containing, StartsWith, EndsWith 메서드 작성

--- src/main/java/com/springboot/board/repository/HospitalRepository.java
+++ src/main/java/com/springboot/board/repository/HospitalRepository.java
@@ -11,4 +11,7 @@ import java.util.List;
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
     Page<Hospital> findAll(Pageable pageable);
     List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+    List<Hospital> findByFullAddressContaining(String keyword);
+    List<Hospital> findByFullAddressStartsWith(String keyword);
+    List<Hospital> findByFullAddressEndsWith(String keyword);
 }
