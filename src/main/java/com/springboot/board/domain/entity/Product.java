1a37f1689c52755934bba311d348c2df29848773
이소영 <th42500@naver.com>
2022-11-14 11:22:54 +0900

    [Add] Product Entity 생성

---
+++ src/main/java/com/springboot/board/domain/entity/Product.java
@@ -0,0 +1,28 @@
+package com.springboot.board.domain.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long number;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
