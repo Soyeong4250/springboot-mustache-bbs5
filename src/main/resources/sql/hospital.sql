cc283f835f5e3d71180325c4d712a48c7501d287
이소영 <th42500@naver.com>
2022-11-14 13:41:08 +0900

    [Docs] 특정 주소와 업태구분명을 이용한 조회 쿼리문  추가

--- src/main/resources/sql/hospital.sql
+++ src/main/resources/sql/hospital.sql
@@ -2,5 +2,9 @@
 select * from `likeliondb`.`nation_wide_hospitals` limit 100;
 
 # 경기도 수원시에 있는 피부과를 조회하는 쿼리문
-select * from `likeliondb`.`nation_wide_hospitals`
-where full_address like '경기도 수원시 %' and hospital_name like '%피부과%';
+select hospital_name, full_address from `likeliondb`.`nation_wide_hospitals`
+where full_address like '경기도 수원시%' and hospital_name like '%피부과%';
+
+# 특정 주소와 업태구분명으로 조회하는 쿼리문
+select business_type_name, hospital_name, full_address from `likeliondb`.`nation_wide_hospitals`
+where business_type_name in ('보건소', '보건지소');
