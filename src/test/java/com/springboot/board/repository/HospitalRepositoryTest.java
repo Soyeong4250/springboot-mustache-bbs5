e1dda3e81f808ee72a6e4de1bfc1778367c2e5af
이소영 <th42500@naver.com>
2022-11-14 14:02:50 +0900

    [Test] 업태구분명으로 조회하는 HospitalRepository 코드 테스트

--- src/test/java/com/springboot/board/repository/HospitalRepositoryTest.java
+++ src/test/java/com/springboot/board/repository/HospitalRepositoryTest.java
@@ -1,14 +1,16 @@
 package com.springboot.board.repository;
 
-import com.springboot.board.domain.Hospital;
+import com.springboot.board.domain.entity.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class HospitalRepositoryTest {
@@ -16,7 +18,7 @@ class HospitalRepositoryTest {
     @Autowired
     HospitalRepository hospitalRepository;
 
-    /*@Test
+    @Test
     @DisplayName("Hospital 정보를 잘 가져오는지 테스트")
     void getHospital() {
         hospitalRepository.findById(1);
@@ -24,6 +26,19 @@ class HospitalRepositoryTest {
         Hospital hp = hospital.get();
         System.out.println(hp.getId());
         assertEquals(1, hp.getId());
-    }*/
+    }
+
+    @Test
+    @DisplayName("BusinessTypeName이 보건소 보건지소 보건진료소인 데이터가 잘 나오는지")
+    void findByBusinessTypeNameIn() {
+        List<String> inClues = new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건지소");
+        inClues.add("보건진료소");
+        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
+        for (var hospital:hospitals) {
+            System.out.println(hospital.getHospitalName());
+        }
+    }
 
 }
