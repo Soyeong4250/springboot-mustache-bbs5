09b9884567b2b8ec23f70ec791f6968ac933851b
이소영 <th42500@naver.com>
2022-11-14 13:17:00 +0900

    [Add] ProductRepository 인터페이스 생성

---
+++ src/main/java/com/springboot/board/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.board.repository;
+
+import com.springboot.board.domain.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+}
