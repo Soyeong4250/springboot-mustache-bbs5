7b32c506f827babd7c947d0b06cf73e2d1bd2dbc
이소영 <th42500@naver.com>
2022-11-15 14:11:03 +0900

    [Add] 레이어드 아키텍처 적용을 위한 Service 클래스 추가

---
+++ src/main/java/com/springboot/board/service/HospitalService.java
@@ -0,0 +1,33 @@
+package com.springboot.board.service;
+
+import com.springboot.board.domain.dto.HospitalResponseDto;
+import com.springboot.board.domain.entity.Hospital;
+import com.springboot.board.repository.HospitalRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class HospitalService {
+
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalService(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    public HospitalResponseDto getHospital(Integer id) {
+
+        Optional<Hospital> optHospital = hospitalRepository.findById(id);  // Entity
+        Hospital hospital =  optHospital.get();
+        HospitalResponseDto hospitalResponseDto = Hospital.of(hospital);  // DTO
+
+        switch (hospital.getBusinessStatusCode()) {
+            case 13: hospitalResponseDto.setBusinessStatusName("영업중"); break;
+            case 3: hospitalResponseDto.setBusinessStatusName("폐업"); break;
+            default: String.valueOf(hospital.getBusinessStatusCode()); break;
+        }
+
+        return hospitalResponseDto;
+    }
+}
