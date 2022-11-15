b7789847c58b41362bafa929428e6f85371c50e2
이소영 <th42500@naver.com>
2022-11-15 14:11:27 +0900

    [Refactor] HospitalController 레이어드 아키텍처 적용

--- src/main/java/com/springboot/board/controller/HospitalController.java
+++ src/main/java/com/springboot/board/controller/HospitalController.java
@@ -3,6 +3,7 @@ package com.springboot.board.controller;
 import com.springboot.board.domain.dto.HospitalResponseDto;
 import com.springboot.board.domain.entity.Hospital;
 import com.springboot.board.repository.HospitalRepository;
+import com.springboot.board.service.HospitalService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,15 +21,15 @@ import java.util.Optional;
 @RequestMapping("/api/v1/hospitals")
 public class HospitalController {
 
-    private final HospitalRepository hospitalRepository;
+    private final HospitalService hospitalService;
 
-    public HospitalController(HospitalRepository hospitalRepository) {
-        this.hospitalRepository = hospitalRepository;
+    public HospitalController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<HospitalResponseDto> get(@PathVariable Integer id) {
-        Optional<Hospital> hospital = hospitalRepository.findById(id);
-        return ResponseEntity.ok().body(Hospital.of(hospital.get()));
+        HospitalResponseDto hospitalResponseDto = hospitalService.getHospital(id);
+        return ResponseEntity.ok().body(hospitalResponseDto);
     }
 }
