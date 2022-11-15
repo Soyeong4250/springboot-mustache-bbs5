54fd96d85a0a805e7291ec9586ed3ba71f6fbff6
이소영 <th42500@naver.com>
2022-11-15 11:22:02 +0900

    [Refactor] Controller 👉 RestController로 변경

--- src/main/java/com/springboot/board/controller/HospitalController.java
+++ src/main/java/com/springboot/board/controller/HospitalController.java
@@ -1,18 +1,23 @@
 package com.springboot.board.controller;
 
+import com.springboot.board.domain.dto.HospitalResponseDto;
 import com.springboot.board.domain.entity.Hospital;
 import com.springboot.board.repository.HospitalRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Optional;
+
+@RestController
 @Slf4j
-@RequestMapping("/hospitals")
+@RequestMapping("/api/v1/hospitals")
 public class HospitalController {
 
     private final HospitalRepository hospitalRepository;
@@ -21,20 +26,9 @@ public class HospitalController {
         this.hospitalRepository = hospitalRepository;
     }
 
-    @GetMapping()
-    public String hospitalsIndex() {
-        return "redirect:/hospitals/list";
-    }
-
-    @GetMapping("/list")
-    public String hospitalList(Pageable pageable, Model model) {
-        log.debug("hospitalList 호출");
-        Page<Hospital> hospitalList = hospitalRepository.findAll(pageable);
-        model.addAttribute("hospitals", hospitalList);
-        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
-        model.addAttribute("next", pageable.next().getPageNumber());
-        model.addAttribute("hasPrev", hospitalList.hasPrevious());
-        model.addAttribute("hasNext", hospitalList.hasNext());
-        return "hospitals/list";
+    @GetMapping("/{id}")
+    public ResponseEntity<HospitalResponseDto> get(@PathVariable Integer id) {
+        Optional<Hospital> hospital = hospitalRepository.findById(id);
+        return ResponseEntity.ok().body(Hospital.of(hospital.get()));
     }
 }
