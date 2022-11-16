39b8d24b98cfb34be256bf204b4be4c47247cb1b
이소영 <th42500@naver.com>
2022-11-16 16:19:25 +0900

    [Add] ArticleRestController 게시글 등록 기능 추가

--- src/main/java/com/springboot/board/controller/ArticleRestController.java
+++ src/main/java/com/springboot/board/controller/ArticleRestController.java
@@ -1,13 +1,11 @@
 package com.springboot.board.controller;
 
+import com.springboot.board.domain.dto.ArticleRequestDto;
 import com.springboot.board.domain.dto.ArticleResponseDto;
 import com.springboot.board.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -23,6 +21,12 @@ public class ArticleRestController {
     @GetMapping("{id}")
     public ResponseEntity<ArticleResponseDto> get(@PathVariable Long id) {
         ArticleResponseDto articleResponseDto = articleService.getArticle(id);
+        return ResponseEntity.ok().body(articleResponseDto);
+    }
+
+    @PostMapping()
+    public ResponseEntity<ArticleResponseDto> register(@RequestBody ArticleRequestDto articleRequestDto) {
+        ArticleResponseDto articleResponseDto = articleService.saveArticle(articleRequestDto);
         return ResponseEntity.ok().body(articleResponseDto);
     }
 }
