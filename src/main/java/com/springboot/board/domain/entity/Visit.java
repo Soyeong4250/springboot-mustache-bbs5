e07855fdac4cdc8a419714eddc09d9bd337d984e
이소영 <th42500@naver.com>
2022-12-07 15:08:23 +0900

    [Feat] Visit Entity 생성

---
+++ src/main/java/com/springboot/board/domain/entity/Visit.java
@@ -0,0 +1,31 @@
+package com.springboot.board.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Visit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String disease;
+    private float amount;
+}
