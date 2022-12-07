0a334c325fed351b4eb4b5579b33fed4487c983d
이소영 <th42500@naver.com>
2022-12-07 22:50:27 +0900

    [Fix] uses unchecked or unsafe operations 경고 해결
    Generic 타입 명시

--- src/main/java/com/springboot/board/domain/entity/Response.java
+++ src/main/java/com/springboot/board/domain/entity/Response.java
@@ -11,10 +11,10 @@ public class Response<T> {
     private T result;
 
     public static Response<Void> error (String resultCode){
-        return new Response(resultCode, null);
+        return new Response<>(resultCode, null);
     }
 
     public static <T> Response<T> success(T result) {
-        return new Response("SUCCESS", result);
+        return new Response<>("SUCCESS", result);
     }
 }
